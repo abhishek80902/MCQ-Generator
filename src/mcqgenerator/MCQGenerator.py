b063eab066238e839f6fc685f76194af4a2e5207
abhishek80902 <kumarabhishek80902@gmail.com>
2026-02-28 23:34:39 +0530

    modal updated

--- src/mcqgenerator/MCQGenerator.py
+++ src/mcqgenerator/MCQGenerator.py
@@ -13,7 +13,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # LLM Setup
 # =========================================================
 llm = ChatOpenAI(
-    model="gpt-3.5-turbo",
+    model="gpt-4o-mini",
     temperature=0.7,
     api_key=OPENAI_API_KEY
 )
